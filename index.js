569e00b8ef0ddfdb21d8d5ee2a61687082ef4fb5
siarheichura <chura14@yandex.ru>
2021-07-29 20:22:11 +0300

    feat: delete storage support and add click sound

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 let value = 0
 
-// Local storage API
-const setLocalStorage = () => {
-    localStorage.setItem('count value', JSON.stringify(value))
-}
-const getLocalStorage = () => {
-    return JSON.parse(localStorage.getItem('count value'))
-}
-
-// DOM-elements
 const counter = document.querySelector('.click-counter__value')
 const buttons = document.querySelector('.btn-group')
-const modal = document.querySelector('.modal')
+const click = document.querySelector('#click')
 
-const setModalText = () => modal.innerText = getLocalStorage() === null ? 'Go-go-go!' : `Your last result: ${getLocalStorage()}`
 const setCounter = () => counter.innerText = value
 
-setTimeout(() => {
-    modal.classList.add('d-none')
-}, 2000);
-
-setModalText()
 setCounter()
 
 buttons.addEventListener('click', event => {
@@ -28,13 +13,15 @@ buttons.addEventListener('click', event => {
     if(target.id === 'up') {
         value += 1
         setCounter()
+        click.play()
     } else if(target.id === 'reset') {
         value = 0
         setCounter()
+        click.play()
     } else if(target.id === 'down') {
+        click.play()
         if(value === 0) return
         value -= 1
         setCounter()
     }
-    setLocalStorage()
 })
